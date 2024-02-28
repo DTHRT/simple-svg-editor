a8060191e165f8ead5a7fd168ec21aff514a670f
Dmitriy Yoo <dmitriy2002yoo@gmail.com>
2024-02-28 12:06:54 +0900

    refactor: don't generate elements in main.js

--- public/main.js
+++ public/main.js
@@ -11,56 +11,6 @@ const delta = {
   y: 0,
 }
 
-const elementsArr = []
-const objectsArr = []
-
-const hexaPolygon = {
-  properties: {
-    fill: 'url(#trama)',
-    points:
-      '70,0 35.00000000000001,60.6217782649107 -34.999999999999986,60.62177826491071 -70,8.572527594031473e-15 -35.00000000000003,-60.621778264910695 35.00000000000001,-60.6217782649107 70,-1.7145055188062946e-14',
-  },
-  tagName: 'polygon',
-  pos: {
-    x: 130,
-    y: 250,
-  },
-}
-objectsArr.push(hexaPolygon)
-
-const pentaPolygon = {
-  properties: {
-    fill: 'url(#trama)',
-    points:
-      '0,-70 66.57395614066074,-21.631189606246316 41.14496766047312,56.63118960624632 -41.144967660473114,56.63118960624632 -66.57395614066075,-21.63118960624631 -1.2858791391047208e-14,-70',
-  },
-  tagName: 'polygon',
-  pos: {
-    x: 400,
-    y: 400,
-  },
-}
-objectsArr.push(pentaPolygon)
-
-const ellipsePath = {
-  properties: {
-    d: 'M-90,0 a90,50 0 1, 0 0,-1  z',
-    fill: 'url(#trama)',
-  },
-  tagName: 'path',
-  pos: {
-    x: 370,
-    y: 150,
-  },
-}
-objectsArr.push(ellipsePath)
-
-for (let i = 0; i < objectsArr.length; i++) {
-  let el = new Element(objectsArr[i], i + 1, svg)
-  el.update()
-  elementsArr.push(el)
-}
-
 svg.addEventListener(
   'mousedown',
   (evt) => {
