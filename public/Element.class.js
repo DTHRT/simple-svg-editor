d0f1a1fc332b7f55b122b69531a2177051ab3883
Dmitriy Yoo <dmitriy2002yoo@gmail.com>
2024-02-28 11:02:02 +0900

    refactor: rewrite Element function to Class

--- public/Element.class.js
+++ public/Element.class.js
@@ -1,111 +1,89 @@
 import { drawElement } from './utils.js'
+
 const SVG_NS = 'http://www.w3.org/2000/svg'
 const deg = 180 / Math.PI
 
-export function Element(o, index, svg) {
-  this.g = document.createElementNS(SVG_NS, 'g')
-  this.g.setAttributeNS(null, 'id', index)
-  svg.appendChild(this.g)
+export class Element {
+  constructor(o, index, svg) {
+    this.g = document.createElementNS(SVG_NS, 'g')
+    this.g.setAttributeNS(null, 'id', index)
+    svg.appendChild(this.g)
 
-  o.parent = this.g
+    o.parent = this.g
 
-  this.el = drawElement(o)
-  this.a = 0
-  this.tagName = o.tagName
-  this.elRect = this.el.getBoundingClientRect()
-  this.svgRect = svg.getBoundingClientRect()
-  this.Left = this.elRect.left - this.svgRect.left
-  this.Right = this.elRect.right - this.svgRect.left
-  this.Top = this.elRect.top - this.svgRect.top
-  this.Bottom = this.elRect.bottom - this.svgRect.top
+    this.el = drawElement(o)
+    this.a = 0
+    this.tagName = o.tagName
+    this.elRect = this.el.getBoundingClientRect()
+    this.svgRect = svg.getBoundingClientRect()
+    this.Left = this.elRect.left - this.svgRect.left
+    this.Right = this.elRect.right - this.svgRect.left
+    this.Top = this.elRect.top - this.svgRect.top
+    this.Bottom = this.elRect.bottom - this.svgRect.top
 
-  this.LT = {
-    x: this.Left,
-    y: this.Top,
-  }
-  this.RT = {
-    x: this.Right,
-    y: this.Top,
-  }
-  this.LB = {
-    x: this.Left,
-    y: this.Bottom,
-  }
-  this.RB = {
-    x: this.Right,
-    y: this.Bottom,
-  }
-  this.c = {
-    x: 0, //(this.elRect.width / 2) + this.Left,
-    y: 0, //(this.elRect.height / 2) + this.Top
-  }
-  this.o = {
-    x: o.pos.x,
-    y: o.pos.y,
+    this.LT = {
+      x: this.Left,
+      y: this.Top,
+    }
+    this.RT = {
+      x: this.Right,
+      y: this.Top,
+    }
+    this.LB = {
+      x: this.Left,
+      y: this.Bottom,
+    }
+    this.RB = {
+      x: this.Right,
+      y: this.Bottom,
+    }
+    this.c = {
+      x: 0, // (this.elRect.width / 2) + this.Left,
+      y: 0, // (this.elRect.height / 2) + this.Top
+    }
+    this.o = {
+      x: o.pos.x,
+      y: o.pos.y,
+    }
+
+    this.A = Math.atan2(this.elRect.height / 2, this.elRect.width / 2)
+
+    let box = {
+      properties: {
+        points: this.pointsValue(),
+        fill: 'none',
+        stroke: 'dodgerblue',
+        'stroke-dasharray': '5,5',
+      },
+      parent: this.g,
+      tagName: 'polyline',
+    }
+    this.box = drawElement(box)
+
+    let leftTop = {
+      properties: {
+        cx: this.LT.x,
+        cy: this.LT.y,
+        r: 6,
+        fill: 'blue',
+      },
+      parent: this.g,
+      tagName: 'circle',
+    }
+
+    this.lt = drawElement(leftTop)
   }
 
-  this.A = Math.atan2(this.elRect.height / 2, this.elRect.width / 2)
-
-  this.pointsValue = function () {
+  pointsValue() {
     // points for the box
     return (
-      this.Left +
-      ',' +
-      this.Top +
-      ' ' +
-      this.Right +
-      ',' +
-      this.Top +
-      ' ' +
-      this.Right +
-      ',' +
-      this.Bottom +
-      ' ' +
-      this.Left +
-      ',' +
-      this.Bottom +
-      ' ' +
-      this.Left +
-      ',' +
-      this.Top
+      `${this.Left},${this.Top} ${this.Right},${this.Top} ` +
+      `${this.Right},${this.Bottom} ${this.Left},${this.Bottom} ${this.Left},${this.Top}`
     )
   }
 
-  let box = {
-    properties: {
-      points: this.pointsValue(),
-      fill: 'none',
-      stroke: 'dodgerblue',
-      'stroke-dasharray': '5,5',
-    },
-    parent: this.g,
-    tagName: 'polyline',
-  }
-  this.box = drawElement(box)
-
-  let leftTop = {
-    properties: {
-      cx: this.LT.x,
-      cy: this.LT.y,
-      r: 6,
-      fill: 'blue',
-    },
-    parent: this.g,
-    tagName: 'circle',
-  }
-
-  this.lt = drawElement(leftTop)
-
-  this.update = function () {
-    let transf =
-      'translate(' +
-      this.o.x +
-      ', ' +
-      this.o.y +
-      ')' +
-      ' rotate(' +
-      this.a * deg +
-      ')'
+  update() {
+    let transf = `translate(${this.o.x}, ${this.o.y}) rotate(${this.a * deg})`
     this.el.setAttributeNS(null, 'transform', transf)
     this.box.setAttributeNS(null, 'transform', transf)
     this.lt.setAttributeNS(null, 'transform', transf)
